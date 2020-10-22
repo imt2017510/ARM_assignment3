783a277aa59c3a13aa14cc5fc996b45e3ab41944
imt2017510 <33414553+imt2017510@users.noreply.github.com>
2020-10-22 23:50:49 +0530

    Add files via upload

---
+++ assignment_log.s
@@ -0,0 +1,26 @@
+AREA appcode,CODE,READONLY
+    EXPORT __main
+		ENTRY
+__main  FUNCTION
+          MOV R0,#100;(i value)
+          MOV R1,#0
+          VLDR.F32 S0,=0.5;Holding the sum of series elements 's'
+          VLDR.F32 S1,=0.5;Temp Variable to hold the intermediate series elements 't'
+          VLDR.F32 S2,=0.5;Holding 'x' Value(to converge x<1)
+          VLDR.F32 S3,=1;addition faactor
+          VLDR.F32 S4,=-1;multiplication factor
+          VLDR.F32 S5,=1 ;i value (i=0)
+exp1      CMP R1,R0;Compare 'i' and 'n'
+          BGE exp;if i < n goto LOOP
+          B stop;else goto stop
+exp1      VMUL.F32 S1,S1,S2;t = t*x
+          VMUL.F32 S1,S1,S4;t=-1*t*x
+          VADD.F32 S6,S5,S3;i=i+1
+          VDIV.F32 S7,S3,S6;k=1/i+1
+          VFMA.F32 S0,S1,S7;sum=sum+(-t*x*i)/(i+1)
+          VADD.F32 S5,S5,S3;i=i+1
+          ADD R0,#1
+          B exp
+stop      B stop  ; stop program
+          endfunc
+        end
